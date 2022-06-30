d13d9c1f0606e4f6d8bde0de4eba46ff50a43f84
Songyue Han <songyueh@google.com>
2022-06-30 03:39:19 +0000

    don't skip omx video codecs for ATV S and below
    skip omx video codecs for non-ATV devices launching with Android S and above.
    
    Bug: 228778569
    Test: Manual
    Change-Id: Ib5145f26a690d3999433ae73e94ac4a604f7fbd7

--- media/libstagefright/omx/OMXStore.cpp
+++ media/libstagefright/omx/OMXStore.cpp
@@ -25,6 +25,8 @@
 
 #include <vndksupport/linker.h>
 
+#include <sstream>
+
 #include <dlfcn.h>
 #include <fcntl.h>
 
@@ -99,8 +101,35 @@ void OMXStore::addPlugin(const char *libname) {
     }
 }
 
+static int getFirstApiLevel() {
+    int boardApiLevel = android::base::GetIntProperty("ro.board.first_api_level", 0);
+    if (boardApiLevel != 0) {
+        return boardApiLevel;
+    }
+
+    return android::base::GetIntProperty("ro.product.first_api_level",
+            android_get_device_api_level());
+}
+
+static bool isTV() {
+    static const bool kIsTv = []() {
+        std::string characteristics = android::base::GetProperty("ro.build.characteristics", "");
+        std::stringstream ss(characteristics);
+        for (std::string item; std::getline(ss, item, ','); ) {
+            if (item == "tv") {
+                return true;
+            }
+        }
+        return false;
+    }();
+    return kIsTv;
+}
+
 void OMXStore::addPlugin(OMXPluginBase *plugin) {
     Mutex::Autolock autoLock(mLock);
+
+    bool typeTV = isTV();
+    int firstApiLevel = getFirstApiLevel();
 
     OMX_U32 index = 0;
 
@@ -109,6 +138,23 @@ void OMXStore::addPlugin(OMXPluginBase *plugin) {
     while ((err = plugin->enumerateComponents(
                     name, sizeof(name), index++)) == OMX_ErrorNone) {
         String8 name8(name);
+
+        Vector<String8> roles;
+        OMX_ERRORTYPE err = plugin->getRolesOfComponent(name, &roles);
+        if (err == OMX_ErrorNone) {
+            bool skip = false;
+            for (String8 role : roles) {
+                if (role.find("video_decoder") != -1 || role.find("video_encoder") != -1) {
+                    if (!typeTV && firstApiLevel >= __ANDROID_API_S__) {
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+            if (skip) {
+                continue;
+            }
+        }
 
         if (mPluginByComponentName.indexOfKey(name8) >= 0) {
             ALOGE("A component of name '%s' already exists, ignoring this one.",
